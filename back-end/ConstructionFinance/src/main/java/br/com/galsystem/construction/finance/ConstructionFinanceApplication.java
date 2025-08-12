2dd39248a61b34b409554371f3868eccc124893a
Gabriel Arruda <gabrielarrudalima@gmail.com>
2025-08-12 17:17:30 -0300

    chore: adicionar projeto backend gerado pelo Spring Initializr

---
+++ back-end/ConstructionFinance/src/main/java/br/com/galsystem/construction/finance/ConstructionFinanceApplication.java
@@ -0,0 +1,13 @@
+package br.com.galsystem.construction.finance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConstructionFinanceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConstructionFinanceApplication.class, args);
+	}
+
+}
---
+++ back-end/ConstructionFinance/src/test/java/br/com/galsystem/construction/finance/ConstructionFinanceApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.galsystem.construction.finance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConstructionFinanceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
