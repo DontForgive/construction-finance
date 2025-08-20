1d7230167c8bbd7daac6aefb7f00f590c8fd5849
Gabriel Arruda <act.gabriel@agi.com.br>
2025-08-20 17:31:39 -0300

    Clear Project

--- front-end/src/app/layouts/admin-layout/admin-layout.module.ts
+++ front-end/src/app/layouts/admin-layout/admin-layout.module.ts
@@ -1,16 +1,8 @@
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { CommonModule } from '@angular/common';
-
 import { DashboardComponent } from '../../pages/dashboard/dashboard.component';
 import { UserComponent } from '../../pages/user/user.component';
-import { TableComponent } from '../../pages/table/table.component';
-import { TypographyComponent } from '../../pages/typography/typography.component';
-import { IconsComponent } from '../../pages/icons/icons.component';
-import { MapsComponent } from '../../pages/maps/maps.component';
-import { NotificationsComponent } from '../../pages/notifications/notifications.component';
-import { UpgradeComponent } from '../../pages/upgrade/upgrade.component';
-
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { CategoryComponent } from 'app/pages/category/category.component';
 import { MatPaginatorModule } from '@angular/material/paginator';
@@ -25,7 +17,6 @@ import { ToastrModule } from 'ngx-toastr';
 import { AdminLayoutRoutes } from './admin-layout.routing';
 import { SupplierComponent } from 'app/pages/supplier/supplier.component';
 import { SupplierAddDialogComponent } from 'app/pages/supplier/supplier-add-dialog.component';
-import { ToastService } from 'app/utils/toastr';
 import { PayerAddDialogComponent } from 'app/pages/payer/payer-add-dialog.component';
 import { PayerComponent } from 'app/pages/payer/payer.component';
 import { ExpenseComponent } from 'app/pages/expense/expense.component';
@@ -66,12 +57,6 @@ import { MatNativeDateModule } from '@angular/material/core';
   declarations: [
     DashboardComponent,
     UserComponent,
-    TableComponent,
-    UpgradeComponent,
-    TypographyComponent,
-    IconsComponent,
-    MapsComponent,
-    NotificationsComponent,
 
     /*  MY PAGES E COMPONENTS  */
     CategoryComponent,
