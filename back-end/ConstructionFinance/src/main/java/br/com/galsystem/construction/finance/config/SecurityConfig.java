35cb70659affda375cfb734970a0ef9b10e67e90
Gabriel Arruda <gabrielarrudalima@gmail.com>
2025-08-20 23:11:17 -0300

    Adicionado expenses/*/attachment no security

--- back-end/ConstructionFinance/src/main/java/br/com/galsystem/construction/finance/config/SecurityConfig.java
+++ back-end/ConstructionFinance/src/main/java/br/com/galsystem/construction/finance/config/SecurityConfig.java
@@ -81,6 +81,7 @@ public class SecurityConfig {
                         .requestMatchers(HttpMethod.GET,  "/files/**").permitAll()
                         .requestMatchers(HttpMethod.HEAD, "/files/**").permitAll()
                         .requestMatchers(HttpMethod.POST, "/uploads/**").authenticated()
+                        .requestMatchers(HttpMethod.PUT, "/expenses/*/attachment").authenticated()
                         .requestMatchers("/v3/api-docs/**", "/swagger-ui/**", "/swagger-ui.html").permitAll()
                         .requestMatchers("/", "/index.html", "/index").permitAll()
                         .requestMatchers("/categories").permitAll()
