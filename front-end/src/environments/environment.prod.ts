f3a4c198f41fe6163a4d995e98a1e4824ced3cab
Gabriel Arruda <gabrielarrudalima@gmail.com>
2025-08-20 21:42:19 -0300

    Build production

--- front-end/src/environments/environment.prod.ts
+++ front-end/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  API: 'http://apiconstrucao.galsystems.com.br/',
-  API_NO_BAR: 'http://apiconstrucao.galsystems.com.br'
+  API: 'https://apiconstrucao.galsystems.com.br/',
+  API_NO_BAR: 'https://apiconstrucao.galsystems.com.br'
 };
